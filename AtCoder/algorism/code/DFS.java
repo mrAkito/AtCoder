a947a466f94266c9261968c586e8da07de88c3a3
mrAkito <fa1con_well@outlook.jp>
2023-06-06 15:22:59 +0900

    First DFS by using stack

--- AtCoder/algorism/code/DFS.java
+++ AtCoder/algorism/code/DFS.java
@@ -1,8 +1,63 @@
-public class DFS {
+import java.util.*;
 
-    private static void dfs(int vertex, boolean[] visited)
-    public static void main(String[] args) {
-        int n = 0, m = 0;
-    }
+public class DFS{
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int h = sc.nextInt();
+		int w = sc.nextInt();
+		char[][] map = new char[h][w];
+		boolean[][] searched = new boolean[h][w];
+		int sx = 0, sy = 0, gx = 0, gy = 0;
+		
+		for(int i=0;i<h;i++) {
+			String str = sc.next();;
+			
+			for(int j=0;j<w;j++) {
+				map[i][j] = str.charAt(j);
+				
+				if(map[i][j]=='s') {
+					sx = j;
+					sy = i;
+				}else if(map[i][j] == 'g'){
+					gx = j;
+					gy = i;
+				}
+			}
+		}
+		sc.close();
+
+		int[] dx = {0, 1, 0, -1};
+		int[] dy = {-1, 0, 1, 0};
+
+		Deque<Plot> stack = new ArrayDeque<Plot>();
+		stack.addFirst(new Plot(sx, sy));
+		searched[sy][sx] = true;
+
+		while(!stack.isEmpty()){
+			Plot p = stack.removeFirst();
+			for(int i = 0; i < 4; i++){
+				int x = p.x + dx[i];
+				int y = p.y + dy[i];
+
+				if(x >= 0 && y >= 0 && y < h){
+					stack.addFirst(new Plot(x, y));
+					searched[y][x] = true;
+				}
+			}
+		}
+
+		System.out.println(searched[gy][gx] ? "Yes" : "No");
+	}
 }
-https://atcoder.jp/contests/abc284/submissions/41435714
+
+class Plot{
+	int x;
+	int y;
+
+	Plot(int a, int b){
+		this.x = a;
+		this.y = b;
+	}
+}
